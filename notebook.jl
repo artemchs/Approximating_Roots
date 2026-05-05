b8bc68a4878cc9f494440ed5b5e3805c613a5994
Artem S Chernysh <artemchs123@gmail.com>
2026-05-05 17:36:06 +0200

    Initial commit

---
+++ notebook.jl
@@ -0,0 +1,37 @@
+### A Pluto.jl notebook ###
+# v0.20.24
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4a538dc0-4897-11f1-991c-bd4747535024
+md"# Approximating Roots"
+
+# ╔═╡ 881e4334-1589-4e55-aed6-2cfde12d3dc6
+md"##### We will be using a numerical method called *Netwon-Raphson method*, which is a technique to approximate the solutions to an equation *f(x) = 0*."
+
+# ╔═╡ c81cd7c0-ea2a-4822-a5aa-42ae94c1835c
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.6"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─4a538dc0-4897-11f1-991c-bd4747535024
+# ╟─881e4334-1589-4e55-aed6-2cfde12d3dc6
+# ╠═c81cd7c0-ea2a-4822-a5aa-42ae94c1835c
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
